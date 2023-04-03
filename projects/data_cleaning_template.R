c70192845bc6fdae63f37cbe2a63a597741743e2
Linh Nguyen <nguy4006@umn.edu>
2023-04-03 10:42:48 -0500

    add t-test and reverse score templates

--- projects/data_cleaning_template.R
+++ projects/data_cleaning_template.R
@@ -1,8 +1,9 @@
 #### META ####
 
 # load packages
-library(rio)   # for importing data files
-library(dplyr) # for data cleaning
+library(rio)      # for importing data files
+library(dplyr)    # for data cleaning
+library(codebook) # for reverse scoring
 
 # working directory -> where R is right now
 getwd()
@@ -28,6 +29,56 @@ data <- data %>%
     gender == 3 ~ NA_real_ # NA for real numbers
   ))
 
+#### REVERSE SCORE ####
+
+# create a new variable that is the "reverse" of the original item
+# e.g., Likert scale 1 to 5
+# 1 <-> 5, 2 <-> 4
+# formula: new score = upper bound + 1 - old score
+# 2 = (5 + 1) - 2 = 4
+
+# THE SLOW WAY
+# manually create all reverse items
+data <- data %>%
+  # create a new variable called bfas083R
+  # which is the reverse of the item bfas083
+  mutate(bfas083R = 6 - bfas083)
+
+# THE FAST WAY
+# pull items for current scale
+scale_items <- dict %>%
+  dplyr::filter(scale %in% "BFAS Conscientiousness") %>%
+  pull(variable)
+
+# pull reversed items
+scale_r_items <- dict %>% 
+  dplyr::filter(variable %in% scale_items) %>%
+  dplyr::filter(keying == -1) %>%
+  pull(variable)
+
+# duplicate data to create reversed scores
+temp_r <- data %>% 
+  dplyr::select(all_of(scale_items))
+
+# rename reversed items to end with R
+# the add_R function comes from the codebook package
+temp_r <- temp_r %>%
+  dplyr::rename_at(scale_r_items, add_R)
+
+# reverse items in duplicated data frame
+temp_r <- temp_r %>%  
+  dplyr::mutate_at(vars(matches("\\dR$")),
+                   codebook::reverse_labelled_values)
+
+# reverse item names in dictionary
+# necessary if you are using the FAST WAY to score your scales
+variable <- c(dict$variable) # create a list of variables
+y <- data.frame(matrix(ncol = length(variable), nrow = 0)) # empty dataframe
+colnames(y) <- variable # name empty dataframe with variable list
+y <- y %>% # rename variable list so reversed items end with R
+  rename_at(scale_r_items, add_R)
+dict$variable <- colnames(y) # merge renamed reverse items to dictionary
+
 #### CREATE SCALE SCORES ####
 
 # create a new variable that is the mean of all your items
@@ -46,6 +97,10 @@ data <- data %>%
 # use a dictionary
 
 # create a list of items that belong to your scale
+# REMEMBER: check to see whether or not the dictionary has reverse item names
+#           if it doesn't -> cannot use this method
+#           if you used the FAST WAY to reverse score 
+#           -> your dictionary will have been updated
 consci_items <- dict %>% 
   
   # filter only rows that belong to BFAS Conscientiousness scale
@@ -66,5 +121,5 @@ data$consci <- data %>%
 
 #### DATA EXPLORATION ####
 
-# mean, variability, graphs etc.
-
+# mean, variability, graphs etc. #
+# refer to eda_template.R        #
--- projects/regression_template.R
+++ projects/regression_template.R
@@ -1,23 +1,91 @@
 #### META ####
 
 # load packages
-library(rio)     # for importing data files
-library(dplyr)   # for general wrangling
-library(ggplot2) # for plots
-library(sjPlot)  # for interaction plot
+library(rio)      # for importing data files
+library(dplyr)    # for general wrangling
+library(ggplot2)  # for plots
+library(sjPlot)   # for interaction plot
+library(lmerTest) # for paired linear model
 
 # working directory -> where R is right now
+# navigate to your project folder if needed
 getwd()
 
 # import the data
-data <- rio::import(file = "NICE_data_YZ.sav")
+data <- rio::import(file = "your_data.sav")
 View(data)
 
 # import the dictionary
-dict <- rio::import(file = "dictionary_YZ.xlsx")
+dict <- rio::import(file = "your_dict.csv")
 
 #### CLEANING ####
 # create scale scores etc. as needed #
+# refer to data_cleaning_template.R  #
+
+#### TWO-SAMPLE T-TEST ####
+
+# e.g., Are men taller than women?
+# IV: gender -> grouping variable
+# DV: height
+
+# create sample data
+women <- data.frame(id = c(1:10),
+                    women = rnorm(n = 10, mean = 160, sd = 10))
+men   <- data.frame(id = c(1:10),
+                    men = rnorm(n = 10, mean = 180, sd = 10))
+merged_data <- merge(women, men)
+
+# create an independent-sample t-test
+t_test_inde    <- t.test(
+  # first group
+  x = women,
+  
+  # second group
+  y = men,
+  
+  # specify data frame
+  data = merged_data,
+  
+  # specify that the data are not paired
+  paired = FALSE)
+
+# create a paired-sample t-test
+t_test_paired    <- t.test(
+  # first group
+  x = women,
+  
+  # second group
+  y = men,
+  
+  # specify data frame
+  data = merged_data,
+  
+  # specify that the data are paired
+  paired = TRUE)
+
+# long data structure
+long_data = data.frame(id     = rep(1:50, times = 2),
+                       gender = rep(c("women","men"), each = 50),
+                       height = c(rnorm(n = 50, mean = 165, sd = 10),
+                                  rnorm(n = 50, mean = 170, sd = 10)))
+
+# create the linear model: independent test
+t_test_lm   <- lm(height ~ gender, data = long_data)
+t_test_inde <- t.test(height ~ gender, data = long_data,
+                      # specify independent structure
+                      paired = FALSE)
+# notice the similar t statistic and p-value
+summary(t_test_lm)
+t_test_inde
+
+# create the linear model: paired test
+t_test_lmer   <- lmerTest::lmer(height ~ gender + (1|id), data = long_data)
+t_test_paired <- t.test(height ~ gender, data = long_data, 
+                        # specify paired structure
+                        paired = TRUE)
+# notice the similar t statistic and p-value
+summary(t_test_lmer)
+t_test_paired
 
 #### SIMPLE REGRESSION ####
 
